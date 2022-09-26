f032adafd780cd8d0ded6e7db02183b4e62c4d81
hehehai <riverhohai@gmail.com>
2022-09-26 21:28:17 +0800

    init repo

---
+++ src/contents/plasmo-inline.vue
@@ -0,0 +1,24 @@
+<template>
+  <button>Custom button</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import type { PlasmoContentScript, PlasmoGetInlineAnchor } from "plasmo";
+
+export const config: PlasmoContentScript = {
+  matches: ["https://www.plasmo.com/*"]
+}
+
+export const getInlineAnchor: PlasmoGetInlineAnchor = () =>
+  document.querySelector("#supercharge > h2 > span")
+
+// Use this to optimize unmount lookups
+export const getShadowHostId = () => "plasmo-inline-example-unique-id"
+
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
